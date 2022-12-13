37787100a37137bac8c23cb9b98d0c7f9c4c17e5
Avishka Indula <avishkainduladev@gmail.com>
2022-12-13 21:03:59 +0530

    Sending a POST Http Request
    => creating code for send a post request to api inside saveTodo() method.
    => now whenever we enter a todo from the input field of index.html, it
    *  will save that todo to the database through the rest api.

--- scripts/app.js
+++ scripts/app.js
@@ -10,7 +10,7 @@ const TodosApp = {
   },
   methods: {
     // This holds methods (functions) we wanna execute with help of vue.
-    saveTodo(event) {
+    async saveTodo(event) {
       event.preventDefault();
       // this.newTodo = this.enteredTodoText;
       // this. refers to methods: object.
@@ -56,10 +56,35 @@ const TodosApp = {
         this.editedTodoId = null;
       } else {
         // Creating new todo
+        let response;
+
+        try {
+          response = await fetch("http://localhost:3000/todos", {
+            method: "POST",
+            body: JSON.stringify({
+              text: this.enteredTodoText,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        } catch (error) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        if (!response.ok) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        const responseData = await response.json();
+
         const newTodo = {
           text: this.enteredTodoText,
-          id: new Date().toISOString(),
+          id: responseData.createdTodo.id,
         };
+
         this.todos.push(newTodo);
         // So now when saveTodo is triggered, a newTodo object will be added to this todos: [] array.
         //  [ { text: "text entered in the input field" , id: 2022-12-13T14:48:00.000Z } ]
