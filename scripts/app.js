1fc0208e2cb48b82318b13c211de18be15ceefda
Avishka Indula <avishkainduladev@gmail.com>
2022-12-13 18:25:11 +0530

    Outputting Lists Of Data
    => creating todos [] array and push newTodo to it in app.js
    => adding <li> items in ul in index.html
    => now we wanna tell vue that we wanna create on <li> item
    *  per todos in todos array. We can use v-for vue directive
    *  inside <li> element. v-for will allow us to loop through
    *  elements inside of that.
    => accessing text: key of the array by using {{ todo.text }} in index.html

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,8 @@ const TodosApp = {
   // TodosApp should start with a capital letter.
   data() {
     return {
-      newTodo: "Learn Vue.js!",
+      // newTodo: "Learn Vue.js!",
+      todos: [],
       enteredTodoText: "",
     };
   },
@@ -10,7 +11,7 @@ const TodosApp = {
     // This holds methods (functions) we wanna execute with help of vue.
     saveTodo(event) {
       event.preventDefault();
-      this.newTodo = this.enteredTodoText;
+      // this.newTodo = this.enteredTodoText;
       // this. refers to methods: object.
       // but if you use this. inside of a method that is registered in this
       // methods object, vue will wire it  up to the data(){} object instead.
@@ -20,10 +21,22 @@ const TodosApp = {
       // So when we hit save button and submit the form, the text will be changed
       // in all places of index.html where we use {{ newTodo }}
 
-      // this.newTodo = this.enteredTodoText; will assign what we've typed in 
+      // this.newTodo = this.enteredTodoText; will assign what we've typed in
       // the input field to the <h2> and <p> elements.
-      this.enteredTodoText = "";
+
+      // this.enteredTodoText = "";
       // This will clear the input field after the form is submitted.
+
+      const newTodo = {
+        text: this.enteredTodoText,
+        id: new Date().toISOString(),
+      };
+      this.todos.push(newTodo);
+      // So now when saveTodo is triggered, a newTodo object will be added to this todos: [] array.
+      //  [ { text: "text entered in the input field" , id: 2022-12-13T14:48:00.000Z } ]
+      // We've accessed this text: key when we've wrote { todo.text }} in index.html
+
+      this.enteredTodoText = "";
     },
   },
 };
